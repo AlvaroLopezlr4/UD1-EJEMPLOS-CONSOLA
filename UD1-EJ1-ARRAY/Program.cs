94fcf978d76452afb9da78f411814f1d06a2fcff
AlvaroL <alvarlopez99@alumnos.alborfp.com>
2026-09-17 18:27:25 +0200

    Agregar archivos de proyecto.

---
+++ UD1-EJ1-ARRAY/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD1_EJ1_ARRAY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variable
+            String[] people = new String[3];
+            int count = 0;
+
+            while (count < people.Length)
+            {
+                Console.WriteLine($"Escribe la palabra numero: {count+1}");
+                people[count] = Console.ReadLine();
+                count++;
+            }
+
+            //Mostramos los datos 
+            foreach (String i in people) {
+                Console.WriteLine(i);
+            }
+        } 
+    }
+}
